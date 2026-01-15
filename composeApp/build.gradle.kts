77509bdcb49be55e40ab305bbfa8abec24673f9a
rintarotajima <rintarotajima@gmail.com>
2026-01-15 21:50:31 +0900

    chore: アプリのバージョン変更[1.0.5→1.0.6]

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -71,8 +71,8 @@ android {
         applicationId = "com.rintaroo.mokumokusolo"
         minSdk = libs.versions.android.minSdk.get().toInt()
         targetSdk = libs.versions.android.targetSdk.get().toInt()
-        versionCode = 7
-        versionName = "1.0.5"
+        versionCode = 8
+        versionName = "1.0.6"
     }
     packaging {
         resources {
