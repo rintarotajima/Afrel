6d5bbd2aff549f4ea4614dbdccc8326c4fb77413
Rintaro Tajima <rintarotajima@gmail.com>
2026-01-29 19:46:22 +0900

    chore: アプリのバージョン変更[1.0.6→1.0.7] (#80)

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -71,8 +71,8 @@ android {
         applicationId = "com.rintaroo.mokumokusolo"
         minSdk = libs.versions.android.minSdk.get().toInt()
         targetSdk = libs.versions.android.targetSdk.get().toInt()
-        versionCode = 8
-        versionName = "1.0.6"
+        versionCode = 9
+        versionName = "1.0.7"
     }
     packaging {
         resources {
